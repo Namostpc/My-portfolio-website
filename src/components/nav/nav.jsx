8b57a8072672c74e0f0eb63c51878d9e403043ff
Namostpc <namorotee@gmail.com>
2025-01-25 14:46:35 +1100

    fix responsive home page

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,39 +1,71 @@
-import React, {useEffect} from "react";
+import React, { useState } from "react";
 import "./nav.scss";
 import Aos from "aos";
-import aosfunc from '../../aos'
+import aosfunc from "../../aos";
+import Hamburger from "hamburger-react";
 function Navbar() {
-
-  aosfunc()
-  return (   
+  aosfunc();
+  const [isOpen, setOpen] = useState(false);
+  return (
     <section id="nav-bar">
-    <div className="nav-container" data-aos="fade-up" data-aos-duration="1000">
-      <div className="logo-container" data-aos="fade-up" data-aos-duration="1000">
-        <a href="/">NAMO</a>
+      <div
+        className="nav-container"
+        data-aos="fade-up"
+        data-aos-duration="1000"
+      >
+        <div
+          className="logo-container"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+        >
+          <a href="/">NAMO</a>
+        </div>
+        <ul className="nav-list">
+          <li>
+            <a href="#nav-bar">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experiences">Experiences</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#skills">Skills</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+        {/* <div className="hamburger-container">
+          <Hamburger toggled={isOpen} toggle={setOpen} />
+          {isOpen && (
+            <ul className="hamburger-list">
+              <li>
+                <a href="#nav-bar">Home</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#experiences">Experiences</a>
+              </li>
+              <li>
+                <a href="#projects">Projects</a>
+              </li>
+              <li>
+                <a href="#skills">Skills</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          )}
+        </div> */}
       </div>
-      <ul className="nav-list">
-        <li>
-          <a href="#nav-bar">Home</a>
-        </li>
-        <li>
-          <a href="#about" onClick="document.getElementby">
-            About
-          </a>
-        </li>
-        <li>
-          <a href="#experiences">Experiences</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="#skills">Skills</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-    </div>
     </section>
   );
 }
